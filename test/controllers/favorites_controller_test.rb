40b7c3aa8da487402de7426754a866f44fe2a2b9
hirosaki ryo <rih60909@gmail.com>
2021-04-28 11:44:12 +0900

    model/controller追加

---
+++ test/controllers/favorites_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FavoritesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get favorites_index_url
+    assert_response :success
+  end
