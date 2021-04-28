21142beb6bf590cf4ab5721becbce0180d15e333
hirosaki ryo <rih60909@gmail.com>
2021-04-28 12:13:20 +0900

    Routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+  resources :favorites, only: [:index, :create, :destroy]
+  resources :picture_blogs do
+    collection do
+      post :confirm
+  end
 end
