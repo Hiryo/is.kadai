fb211c02b256f61c2902f82f4c0d73d2971de60f
hirosaki ryo <rih60909@gmail.com>
2021-04-29 20:04:38 +0900

    お気に入り機能追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,14 @@
 class FavoritesController < ApplicationController
   def index
   end
+
+  def create
+    favorite = current_user. favorites.create(blog_id: params[:blog_id])
+    redirect_to blogs_path, notice: "#{favorite.blog.user.name}'s blogs is added to your favorite"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).def destroy
+    redirect_to blogs_path, notice: "#{favorite.blog.user.name}'s blogs is removed from your favorite"
+  end
 end
