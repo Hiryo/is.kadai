b67288605861843a4ea2375068d63951b68ead27
hiryo <rih60909@gmail.com>
2021-05-01 20:06:00 +0900

    辞書機能追加、国際化仕様

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,5 +3,7 @@ class Blog < ApplicationRecord
   has_many :favorite_users, through: :favorites, source: :user
   belongs_to :user
 
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { maximum: 100 }
   mount_uploader :image, ImageUploader
 end
